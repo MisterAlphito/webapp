69e88e1d616257abb49b1a03d7f67d6df38bc070
MisterAlphito <38398284+MisterAlphito@users.noreply.github.com>
2025-01-09 13:19:16 +0100

    portfolio22
    +Finalización de contenidos de textos
    +Arreglos semánticos
    +Añadidos alt para imágenes
    +Limpieza de comentarios
    +Reducción del tamaño del logo de la landing page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// CERRAR MENU EN CLICK
-
 //CORTINILLA
 
 const imagenes = document.querySelector(".imagenes");
@@ -27,6 +25,26 @@ function avanzarCortinilla() {
 
 setInterval(avanzarCortinilla, 3000);
 
+// Parallax cortinilla
+
+const cortinillas = document.querySelectorAll(".cortinilla");
+
+cortinillas.forEach((item) => {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: item,
+        start: "top 99%",
+        end: "top 50%",
+        scrub: true,
+      },
+    })
+    .to(item, {
+      height: "90vh", // Altura final
+      ease: "none",
+    });
+});
+
 //MENU
 gsap.registerPlugin(ScrollTrigger);
 
@@ -41,41 +59,38 @@ let isRotated = false;
 
 menuIcon.addEventListener("click", () => {
   if (isRotated) {
-    // Restaura la rotación a 0 grados
     gsap.to(menuIcon, {
       rotation: 0,
       duration: 0.3,
       ease: "power1.out",
     });
   } else {
-    // Rota el elemento 90 grados
     gsap.to(menuIcon, {
       rotation: 90,
       duration: 0.3,
       ease: "power1.out",
     });
   }
-  isRotated = !isRotated; // Alterna el estado de la rotación
+  isRotated = !isRotated;
 });
 
-// Asegúrate de que GSAP y ScrollTrigger están correctamente registrados
+// Parallax textos pequeños
+
 gsap.registerPlugin(ScrollTrigger);
 
-// Selección de todos los divs interactivos
-const interactiveDivs = document.querySelectorAll(".interactive-div");
+const interactiveDivs = document.querySelectorAll(".div-parallax");
 
 interactiveDivs.forEach((div) => {
   const description = div.querySelector(".descripcion-1");
-  const span = div.querySelector(".h2-animado-1");
+  const span = div.querySelector(".h2-animado");
 
-  // Configuración de GSAP + ScrollTrigger para cada elemento
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: div, // Cada div interactivo tiene su propio trigger
-        start: "top 80%", // Comienza cuando el top del div llega al 75% de la pantalla
-        end: "bottom 70%", // Termina cuando el bottom del div llega al 65% de la pantalla
-        scrub: 0.1, // Sincroniza con el scroll
+        trigger: div,
+        start: "top 80%",
+        end: "bottom 70%",
+        scrub: 0.1,
         onUpdate: (self) => {
           // Se ejecuta cada vez que el scroll se actualiza
           const currentPosition = div.getBoundingClientRect(); // Obtener la posición actual del div
@@ -84,20 +99,19 @@ interactiveDivs.forEach((div) => {
             currentPosition
           );
 
-          // Aquí es donde realmente puedes hacer que las animaciones se actualicen dinámicamente
-          const progress = self.progress; // La posición del trigger con respecto al desplazamiento
+          const progress = self.progress;
           console.log("Progress", progress);
         },
       },
     })
-    // Animación del span (h2)
+
     .to(span, {
       y: -44, // Movimiento hacia arriba
       opacity: 1,
-      duration: 0.01, // Duración suave
+      duration: 0.01,
       ease: "none",
     })
-    // Animación de la descripción
+
     .to(
       description,
       {
@@ -106,17 +120,19 @@ interactiveDivs.forEach((div) => {
         duration: 0.01,
         ease: "none",
       },
-      "<" // Se ejecuta al mismo tiempo que la animación anterior
+      "<"
     );
 });
 
-// Seleccionar los elementos grid-item
+// GALERÍA
+
+// Interacciones galería
+
 const gridItems = document.querySelectorAll(".grid-item");
 
-// Iterar sobre cada grid-item y aplicar la animación con GSAP
 gridItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
-    // Cambiar el fondo a amarillo cuando el cursor entra
+    // Cambiar el fondo a azul cuando el cursor entra
     gsap.to(item, { backgroundColor: "#577e80", duration: 0.55 });
   });
 
@@ -126,7 +142,7 @@ gridItems.forEach((item) => {
   });
 });
 
-// Agregar animaciones al hover
+// Animaciones con hover
 gridItems.forEach((item) => {
   const h2 = item.querySelector("h2");
 
@@ -149,15 +165,16 @@ gridItems.forEach((item) => {
   });
 });
 
+// Parallax galería
+
 gridItems.forEach((item) => {
-  // Configuración de GSAP + ScrollTrigger
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: item, // Cada .grid-item será el activador
-        start: "top 93%", // Cuando el elemento entra en la vista
-        end: "top 70%", // Cuando el elemento llega al centro de la pantalla
-        scrub: true, // Proporcional al scroll
+        trigger: item,
+        start: "top 93%",
+        end: "top 70%",
+        scrub: true,
       },
     })
     .to(item, {
@@ -166,61 +183,38 @@ gridItems.forEach((item) => {
     });
 });
 
-// Selección de elementos .grid-item h2
 const gridItemHeadings = document.querySelectorAll(".grid-item h2");
 
 gridItemHeadings.forEach((heading) => {
-  // Configuración de GSAP + ScrollTrigger
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: heading, // Cada h2 dentro de .grid-item será el activador
-        start: "top 93%", // Cuando el elemento entra en la vista
-        end: "top 70%", // Cuando el elemento llega al centro de la pantalla
-        scrub: true, // Proporcional al scroll
+        trigger: heading,
+        start: "top 93%",
+        end: "top 70%",
+        scrub: true,
       },
     })
     .to(heading, {
-      fontSize: "2em", // Tamaño final de la fuente
-      ease: "none", // Movimiento directo, sin suavidad
+      fontSize: "2em", // Tamaño final de la letra
+      ease: "none",
     });
 });
 
-// Seleccionar todos los elementos con la clase .cortinilla
-const cortinillas = document.querySelectorAll(".cortinilla");
+// Parallax imagenes general
 
-// Iterar sobre cada elemento y aplicar la animación
-cortinillas.forEach((item) => {
-  // Configuración de GSAP + ScrollTrigger
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: item, // Cada .cortinilla será el activador
-        start: "top 99%", // Cuando el elemento entra en la vista (arriba del 95% de la pantalla)
-        end: "top 50%", // Cuando el elemento llega al centro de la pantalla
-        scrub: true, // Hacer la animación proporcional al scroll
-      },
-    })
-    .to(item, {
-      height: "90vh", // Altura final (la animación de altura será a 90vh)
-      ease: "none", // Movimiento directo, sin suavidad
-    });
-});
-
-// Iterar sobre cada elemento y aplicar la animación
 imagenes.forEach((item) => {
-  // Configuración de GSAP + ScrollTrigger
   gsap
     .timeline({
       scrollTrigger: {
-        trigger: item, // Cada .imagenes será el activador
-        start: "top 99%", // Cuando el elemento entra en la vista (arriba del 99% de la pantalla)
-        end: "top 50%", // Cuando el elemento llega al centro de la pantalla
-        scrub: true, // Hacer la animación proporcional al scroll
+        trigger: item,
+        start: "top 99%",
+        end: "top 50%",
+        scrub: true,
       },
     })
     .to(item, {
-      height: "100vh", // Altura final (la animación de altura será a 90vh)
-      ease: "none", // Movimiento directo, sin suavidad
+      height: "100vh", // Altura final
+      ease: "none",
     });
 });
